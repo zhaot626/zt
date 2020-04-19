b89852045cf99f10d36f690a8f530b0c288cd708
zhaot626 <1653013883@qq.com>
2020-04-19 15:50:58 +0800

    创建本地仓

---
+++ src/main/java/com/mr/music1/Music1Application.java
@@ -0,0 +1,13 @@
+package com.mr.music1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Music1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Music1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mr/music1/Music1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mr.music1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Music1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
